619ed1d9e63e7e53371e2e9507ac134a3457731b
MARLO <marlo_rebaza@hotmail.com>
2019-06-16 21:43:40 -0500

    Commit inicial

---
+++ Basico.py
@@ -0,0 +1,5 @@
+
+
+if __name__ == '__main__':
+    print(":D")
+    # ACA ME QUEDO https://platzi.com/clases/1378-python/14084-operadores-matematicos4235/
---
+++ Tortuga.py
@@ -0,0 +1,22 @@
+import turtle
+
+
+if __name__ == '__main__':
+
+    window = turtle.Screen()
+    tortuga = turtle.Turtle()
+    
+    tortuga.forward(100) # avanza 100 pixeles
+    tortuga.right(90) # derecha 90º
+    
+    tortuga.forward(100)
+    tortuga.right(90)
+    
+    tortuga.forward(100)
+    tortuga.right(90)
+    
+    tortuga.forward(100)
+    tortuga.right(90)
+    
+    
+    window.mainloop()
