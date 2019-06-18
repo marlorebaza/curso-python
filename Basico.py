efb71e140aac6581df0e81c51af949311e6864c0
MARLO <marlo_rebaza@hotmail.com>
2019-06-17 23:56:13 -0500

    avance operadores matematicos

--- Basico.py
+++ Basico.py
@@ -1,5 +1,22 @@
 
 
 if __name__ == '__main__':
-    print(":D")
+    '''
+        Operadores matematicos
+    '''
+    # suma
+    print(1 + 1) # 2
+    # resta 
+    print(10 - 1) # 9 
+    # multiplicacion
+    print(10 * 2)  # 20
+    # division
+    print(11 / 4) # 2.75
+    # division de enteros (excluye decimales)
+    print(11 // 4) # 2 
+    # residuo
+    print(3 % 2) # 1
+    # exponencia
+    print(2**3) # 8
     # ACA ME QUEDO https://platzi.com/clases/1378-python/14084-operadores-matematicos4235/
+    # minuto 5:15
